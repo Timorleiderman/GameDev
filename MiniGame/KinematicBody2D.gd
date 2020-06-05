94621f27005244deaa30b45b750a7a8e17bb1d6c
Timorleiderman <Timorleiderman@gmail.com>
2020-06-05 15:10:22 +0300

    Timor: MiniGame autotile map and camera

--- MiniGame/KinematicBody2D.gd
+++ MiniGame/KinematicBody2D.gd
@@ -1,9 +1,10 @@
 extends KinematicBody2D
 
 const UP = Vector2(0,-1)
-const GRAVITY = 20
-const SPEED = 200
-const JUMP_HEIGHT = 700
+const GRAVITY = 15
+const MAX_SPEED = 250
+const ACCELERATION = 20
+const JUMP_HEIGHT = 500
 
 var motion = Vector2() # x and y into a single var
 
@@ -18,19 +19,35 @@ func _ready():
 
 func _physics_process(delta):
 	
+	var friction = false
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x+ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x-ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
-		motion.x = 0
-	
-	if is_on_floor():
+		$Sprite.play("Idle")
+		friction = true
 		
+		
+	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -JUMP_HEIGHT
-			#$Sprite.frame +=1
+		if friction == true:
+			motion.x = lerp(motion.x, 0 , 0.2) # from, to, wheight -> this is friction
+			
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0 , 0.05) # from, to, wheight -> this is friction
+		
 	motion = move_and_slide(motion, UP)
 	pass
